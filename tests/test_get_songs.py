d04d53833e130d21846b777a3d3351df27ce7cda
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2022-03-27 22:54:42 +0300

    Add the negative tests

--- tests/test_get_songs.py
+++ tests/test_get_songs.py
@@ -54,6 +54,31 @@ def test_get_songs(client, song_number, limit, offset):
     expected_songs = created_songs[offset:max_expected_index]
     query_params = {"limit": limit, "offset": offset}
     response = client.get("/api/v1/song", query_string=query_params)
+    assert response.status_code == 200
     received_result = response.get_json()
     diff = DeepDiff(received_result, {"data": expected_songs})
     assert not diff.to_dict()
+
+
+@pytest.mark.parametrize(
+    ("limit", "offset"),
+    [
+        ("invalid limit", 5),
+        (2, "invalid_offset"),
+        (DEFAULT_LIMIT + 1, 2),
+        (0, 3),
+        (5.14, 9.8)
+    ],
+    ids=[
+        "string limit",
+        "string offset",
+        "too big limit",
+        "too small limit",
+        "float limit and offset"
+    ]
+)
+def test_get_songs_error(client, limit, offset):
+    query_params = {"limit": limit, "offset": offset}
+    response = client.get("/api/v1/song", query_string=query_params)
+    assert response.status_code == 400
+    print(response.data.decode())
