19843c1ec16c1f2654a711ec9d8d60507238dde1
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2022-04-07 00:49:58 +0300

    add the tests checking a difficulty per level

--- tests/test_get_average_difficulty.py
+++ tests/test_get_average_difficulty.py
@@ -23,3 +23,33 @@ def test_get_average_difficulty(client, difficulties, expected_avg_difficulty):
     response = client.get("/api/v1/difficulty")
     response_body = response.get_json()["data"]
     assert response_body["average_difficulty"] == expected_avg_difficulty
+
+
+@pytest.mark.parametrize(
+    ("song_features", "request_level", "expected_avg_difficulty"),
+    [
+        ([], 5, None),
+        ([(10, 5)], 5, 10),
+        ([(10, 5)], 1, None),
+        ([(10, 5), (15, 5), (3, 1)], 5, 12.5)
+    ],
+    ids=[
+        "no songs",
+        "one song",
+        "one songs, unsuitable level",
+        "three songs"
+    ]
+)
+def test_get_average_difficulty_per_level(
+        client,
+        song_features,
+        request_level,
+        expected_avg_difficulty
+):
+    for difficulty, level in song_features:
+        add_song(difficulty=difficulty, level=level)
+
+    query_params = {"level": request_level}
+    response = client.get("/api/v1/difficulty", query_string=query_params)
+    response_body = response.get_json()["data"]
+    assert response_body["average_difficulty"] == expected_avg_difficulty
